4c6bb487c408b4309213ddc2d16f0fc8b5a3781e
kaforce80 <kaforce80@gmail.com>
2026-04-17 15:59:00 +0900

    Migrate admin auth to SQLite and add authenticator setup

--- Widiny.Stock.Web/Program.cs
+++ Widiny.Stock.Web/Program.cs
@@ -1,8 +1,49 @@
+using Widiny.Stock.Web.Data;
+using Widiny.Stock.Web.Models.Auth;
+using Widiny.Stock.Web.Services.Auth;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<AdminAuthOptions>(builder.Configuration.GetSection(AdminAuthOptions.SectionName));
+
+builder.Services.AddDbContext<StockDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Widiny.StockDB") ?? "Data Source=Widiny.StockDB.db"));
+
+builder.Services.AddScoped<AdminAccountService>();
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.AccessDeniedPath = "/Account/Login";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+});
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<StockDbContext>();
+    dbContext.Database.EnsureCreated();
+
+    var adminService = scope.ServiceProvider.GetRequiredService<AdminAccountService>();
+    await adminService.EnsureSeedAdminAsync();
+}
 
 if (!app.Environment.IsDevelopment())
 {
@@ -14,6 +55,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
